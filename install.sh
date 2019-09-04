54754e60fd34d2ba0f80cde71bdb796e7a9ece0c
Sławomir Paszko <folkus@gmail.com>
2019-09-04 09:18:49 +0200

    Installation fix.

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+dir=$(pwd)
  pip install pandas-datareader
  pip install pandas
  location=$(pip show pandas | grep Locat | grep " .*" -o)
  file=$(find ${location} | grep "/stooq.py$")
  gvim ${file}
-# sudo ln -sf ./stock-viever.py 
+
+ # commands install
+ cd /usr/bin
+ sudo ln -sf ${dir}/stock-viewer.py stock-viewer
