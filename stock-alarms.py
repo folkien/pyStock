60484979e6eb17252ee1415c8cd4f46e0b27894e
Sławomir Paszko <folkus@gmail.com>
2019-09-04 11:51:23 +0200

    First version of alarams added.

--- stock-alarms.py
+++ stock-alarms.py
@@ -3,48 +3,62 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sys, argparse
 import datetime
+import json
+import os
 from pandas_datareader import data
 
+alarmsConfigFile="config/alarms.json"
+alarms=[]
+
+def alarmsRead():
+    global alarmsConfigFile
+    global alarms
+    if os.path.isfile(alarmsConfigFile):
+        with open(alarmsConfigFile, 'r') as f:
+            alarms = json.load(f)
+
+def alarmsWrite():
+    global alarmsConfigFile
+    global alarms
+    with open(alarmsConfigFile, 'w') as f:
+        json.dump(alarms, f)
+
+def alarmsAdd(name,reference,alarmType,value):
+    global alarms
+    alarms.append({"name":name, "reference":reference, "type":alarmType, "value":value})
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--stockCode", type=str, required=True, help="Stock name code")
-parser.add_argument("-d", "--beginDate", type=str, required=False, help="Begin date")
-parser.add_argument("-a", "--averageDays", type=int, required=False, help="Day to calc mean")
-parser.add_argument("-Y", "--lastYear", action='store_true', required=False, help="Last Year")
-parser.add_argument("-M", "--lastMonth", action='store_true', required=False, help="Last Month")
+parser.add_argument("-a", "--addAlarm", action='store_true', required=False, help="")
+parser.add_argument("-n", "--stockCode", type=str, required=False, help="")
+parser.add_argument("-r", "--referencePrice", type=int, required=False, help="")
+parser.add_argument("-t", "--type", type=str, required=False, help="")
+parser.add_argument("-v", "--value", type=int, required=False, help="")
 parser.add_argument("-W", "--lastWeek", action='store_true', required=False, help="Last Week")
-parser.add_argument("-g", "--plotToFile", action='store_true', required=False, help="Plot to file")
 args = parser.parse_args()
 
 #Assert
-if (not args.stockCode):
-    print "No stockCode!"
+if (not args.addAlarm):
+    print "Missing event"
     sys.exit(1)
 
-if (not args.averageDays):
-    args.averageDays=30
+if (args.addAlarm):
+    if (not args.stockCode or not args.referencePrice or not args.type or not args.value):
+        print "Missing arguments for adding."
+        sys.exit(1)
 
 
-# Dates
-currentDateTime = datetime.datetime.now()
 ## End date
+currentDateTime = datetime.datetime.now()
 end_date    =  currentDateTime.strftime("%Y-%m-%d")
-## Start date
-if (args.beginDate):
-    start_date  = args.beginDate
-else:
-    start_date  = '2010-01-01'
-# Check last year
-if (args.lastYear):
-    tmpDate = datetime.datetime.now() - datetime.timedelta(days=365)
-    start_date  =  tmpDate.strftime("%Y-%m-%d")
 # Check last month
-if (args.lastMonth):
-    tmpDate = datetime.datetime.now() - datetime.timedelta(days=30)
-    start_date  =  tmpDate.strftime("%Y-%m-%d")
-# Check last Week
-if (args.lastWeek):
-    tmpDate = datetime.datetime.now() - datetime.timedelta(days=7)
-    start_date  =  tmpDate.strftime("%Y-%m-%d")
+tmpDate = datetime.datetime.now() - datetime.timedelta(days=30)
+start_date  =  tmpDate.strftime("%Y-%m-%d")
+
+alarmsRead()
+if (args.addAlarm):
+    alarmsAdd(args.stockCode,args.referencePrice,args.type,args.value)
+    alarmsWrite()
+
 
 # 1. Get DATA from URL
 # #####################################################33
@@ -58,40 +72,4 @@ if len(panel_data) == 0:
 # Getting just the adjusted closing prices. This will return a Pandas DataFrame
 # The index in this DataFrame is the major index of the panel_data.
 close = panel_data['Close']
-
-# 2. Reindex data and calculate average
-# #####################################################33
-# Getting all weekdays between 01/01/2000 and 12/31/2016
-all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
-
-# How do we align the existing prices in adj_close with our new set of dates?
-# All we need to do is reindex close using all_weekdays as the new index
-close = close.reindex(all_weekdays)
-
-# Reindexing will insert missing values (NaN) for the dates that were not present
-# in the original set. To cope with this, we can fill the missing by replacing them
-# with the latest available price for each instrument.
-close = close.fillna(method='ffill')
-
-# Get the MSFT timeseries. This now returns a Pandas Series object indexed by date.
-msft = close
-
-# Calculate the days moving averages
-average = msft.rolling(window=int(args.averageDays),min_periods=1).mean()
-
-
-# 3. Plot data
-# #####################################################33
-plt.plot(msft.index, msft, label=args.stockCode)
-plt.plot(average.index, average, label=str(args.averageDays)+' days mean')
-plt.xlabel('Date')
-plt.ylabel('Closing price (zl)')
-plt.grid()
-plt.legend()
-if (args.plotToFile):
-    outputFilename="plots/"+args.stockCode+"."+end_date+"plot.png"
-    plt.savefig(outputFilename)
-    print outputFilename
-else:
-    plt.show()
 
