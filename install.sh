6b09e787297e9798e7eb179a5829a974e73dbf75
Sławomir Paszko <folkus@gmail.com>
2019-09-07 00:14:42 +0200

    Added tktinkter.

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ dir=$(pwd)
  pip install pandas-datareader
  pip install pandas
  pip install matplotlib
+ sudo apt-get install python-tk
  location=$(pip show pandas | grep Locat | grep " .*" -o)
  file=$(find ${location} | grep "/stooq.py$")
  gvim ${file}
