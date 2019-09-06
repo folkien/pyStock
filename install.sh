9f8c72249ba6b155542b53aa48e7a7d119eb5908
Sławomir Paszko <folkus@gmail.com>
2019-09-07 00:12:44 +0200

    Instal fix.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 dir=$(pwd)
  pip install pandas-datareader
  pip install pandas
+ pip install matplotlib
  location=$(pip show pandas | grep Locat | grep " .*" -o)
  file=$(find ${location} | grep "/stooq.py$")
  gvim ${file}
