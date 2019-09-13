cbb7e61abd6927d80c7027093786b9b7920376eb
Sławomir Paszko <folkus@gmail.com>
2019-09-14 00:35:30 +0200

    install fix.

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 dir=$(pwd)
+
+stock-alarms -c > /dev/null
+if [ $? -ne 0 ]; then
  pip install pandas-datareader
  pip install pandas
  pip install matplotlib
@@ -6,7 +9,9 @@ dir=$(pwd)
  location=$(pip show pandas | grep Locat | grep " .*" -o)
  file=$(find ${location} | grep "/stooq.py$")
  gvim ${file}
+fi
 
  # commands install
  cd /usr/bin
  sudo ln -sf ${dir}/stock-viewer.py stock-viewer
+ sudo ln -sf ${dir}/stock-alarms.py stock-alarms
