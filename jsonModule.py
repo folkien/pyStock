8b61cfc4d99bf12c8d19104702d60ca40b10937b
Sławomir Paszko <folkus@gmail.com>
2020-01-03 13:57:33 +0100

    Added stock viewer manager.

---
+++ jsonModule.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2.7
+'''
+Created on 3 sty 2020
+
+@author: spasz
+'''
+import json
+import os
+
+def jsonRead(filename):
+    data = []
+    if os.path.isfile(filename):
+        with open(filename, 'r') as f:
+            data = json.load(f)
+    return data
+
+def jsonWrite(filename,data):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4, sort_keys=True)
+    print("Written %s.\n" % (filename))
+
+def jsonShow(data):
+    for entry in data:
+        print entry
---
+++ stock-viewer-manager.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python2.7
+import pandas as pd
+import sys, argparse
+import datetime
+import json
+import os
+from jsonModule import *
+
+configFile="config/viewer.json"
+dataIsChanged=False
+entries=[]
+
+# Entry handling
+def entryAdd(arguments,url):
+    global entries
+    entries.append({"arguments":arguments, "url":url })
+
+def entryRemove(arguments, url):
+    global entries
+    try:
+        entries.remove({"arguments":arguments, "url":url })
+    except ValueError:
+        pass
+
+def entryPrint(entry):
+    print entry
+
+def entryCheck(value,alarm):
+    return False
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--add",    action='store_true', required=False, help="Adds given")
+parser.add_argument("-d", "--delete", action='store_true', required=False, help="Remove")
+parser.add_argument("-e", "--execute", action='store_true', required=False, help="Execute")
+parser.add_argument("-s", "--show", action='store_true', required=False, help="Print")
+parser.add_argument("-an", "--arguments", type=str, required=False, help="Arguments")
+parser.add_argument("-au", "--url", type=str, required=False, help="Bankier URL")
+args = parser.parse_args()
+
+#Assert
+if (not args.add and not args.execute and not args.delete and not args.show):
+    print "Missing event"
+    sys.exit(1)
+
+entries = jsonRead(configFile)
+
+# 0. Adding entries
+# #####################################################33
+if (args.add):
+    entryRemove(args.arguments, args.url)
+    entryAdd(args.arguments, args.url)
+    dataIsChanged = True
+
+# 1. Removing entries
+# #####################################################33
+if (args.delete):
+    entryRemove(args.arguments, args.url)
+    dataIsChanged = True
+
+# 2. Checking entries
+# #####################################################33
+for i in range(len(entries)):
+    entry = entries[i]
+
+    if (args.show):
+        entryPrint(entry)
+
+    if (args.execute):
+        print "No Stooq data for entry!"
+
+
+# 4. Write entries if were changed
+# #####################################################33
+if (dataIsChanged == True):
+    jsonWrite(configFile, entries)
+
+
