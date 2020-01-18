dbe4122d9638527ae942b8e6035852bc0d5a5053
Sławomir Paszko <folkus@gmail.com>
2020-01-18 23:31:41 +0100

    Added RSI with TODO.

--- stock-viewer.py
+++ stock-viewer.py
@@ -94,6 +94,39 @@ def PlotMACD(macd,signal):
     plt.plot(fromBottom.index, fromBottom, 'ro', label='Buy')
     plt.plot(fromTop.index, fromTop, 'go', label='Sell')
 
+# Set RSI indicator
+# TODO :
+def SetRSI(prices, n=14):
+    deltas = np.diff(prices)
+    seed = deltas[:n+1]
+    up = seed[seed>=0].sum()/n
+    down = -seed[seed<0].sum()/n
+    rs = up/down
+    rsi = np.zeros_like(prices)
+    rsi[:n] = 100. - 100./(1.+rs)
+
+    for i in range(n, len(prices)):
+        delta = deltas[i-1] # cause the diff is 1 shorter
+
+        if delta>0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+
+        up = (up*(n-1) + upval)/n
+        down = (down*(n-1) + downval)/n
+
+        rs = up/down
+        rsi[i] = 100. - 100./(1.+rs)
+
+    return rsi
+
+def PlotRSI(rsi):
+    # TODO
+    return 0
+
 # Calculate diff
 def Diffrentiate(dataset):
     diff=numpy.diff(dataset).tolist()
