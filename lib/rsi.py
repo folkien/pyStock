8c523bb45e9231635c5ac70e5e237889efeb8c7b
Sławomir Paszko <folkus@gmail.com>
2020-02-04 23:29:31 +0100

    Fix.

--- lib/rsi.py
+++ lib/rsi.py
@@ -20,13 +20,13 @@ class RSI:
             self.n     = n
             self.overBoughtLvl = 70
             self.overSellLvl   = 30
-            self.hystersis     = 20
+            self.hystersis     = 5
             self.rsi  = self.InitRSI(prices, self.n)
-            self.buySignal   = 0
-            self.sellSignal  = 0
+            self.buySignal   = CreateSubsetByValues(self.rsi, 0, 30)
+            self.sellSignal  = CreateSubsetByValues(self.rsi, 70, 100)
             self.trendToFall = CreateSubsetByValues(self.rsi, 100-self.hystersis, 100)
             self.trendToRise = CreateSubsetByValues(self.rsi, 0, self.hystersis)
-
+        
         # Set RSI indicator
         def InitRSI(self,prices,n):
             deltas = numpy.diff(prices)
@@ -58,17 +58,23 @@ class RSI:
         # Plot method
         def Plot(self):
             plt.plot(self.rsi.index, self.rsi, label='RSI', color = '#000000')
-            plt.ylim(top=100,bottom=0)
             #OverBought
             overBought = CreateDataLine(self.rsi.index, 70, 70)
             plt.plot(overBought.index, overBought, '--', label='Overbought', color = '#AAAAAA')
             #OverSold
             overSold = CreateDataLine(self.rsi.index, 30, 30)
             plt.plot(overSold.index, overSold, '--', label='Oversold', color = '#AAAAAA')
+            # Buy
+            if (self.buySignal.size):
+                plt.plot(self.buySignal.index, self.buySignal, 'x', label='Buy', color = '#00FF00')
+            # Sell
+            if (self.sellSignal.size):
+                plt.plot(self.sellSignal.index, self.sellSignal, '*', label='Sell', color = '#FF0000')
             # Trend to Fall
             if (self.trendToFall.size):
-                plt.plot(self.trendToFall.index, self.trendToFall, label='ToFall', color = '#FF0000')
+                plt.plot(self.trendToFall.index, self.trendToFall, '*', label='ToFall', color = '#FFFF00')
             # Trend to Rise
             if (self.trendToRise.size):
-                plt.plot(self.trendToRise.index, self.trendToRise, label='ToRise', color = '#00FF00')
+                plt.plot(self.trendToRise.index, self.trendToRise, '*', label='ToRise', color = '#00FFFF')
+            plt.ylim(top=100,bottom=0)
 
