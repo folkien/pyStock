bbb5b1f4473896506615ce2f3bb9aa6b417746c9
Sławomir Paszko <folkus@gmail.com>
2020-02-07 00:04:29 +0100

    ToFix.

--- lib/htmlModule.py
+++ lib/htmlModule.py
@@ -3,6 +3,7 @@
 import  os, urllib, re
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+import urllib
 
 
 "Fetcher gets url data and extracts specified HTML element with specified classes"
@@ -18,32 +19,24 @@ class htmlFetcher:
 
         "Fetches HTML data from given URL"
         def fetchHtmlData(self):
-            from urllib.request import Request, urlopen
-            from urllib.error import URLError, HTTPError
 
-            try:
-                response = urllib.request.urlopen(self.url)
-            except HTTPError as e:
-                print('Error code: ', e.code)
-            except URLError as e:
-                print('Reason: ', e.reason)
+            response = urllib.request.urlopen(self.url)
+            # Get encoding, try utf-8 if not found.
+            self.coding = response.headers.get_content_charset()
+            if (self.coding is None):
+                self.coding = "utf-8"
+
+            # Get data as string
+            if (len(response.read()) != 0):
+                print("Encoding is %s" % self.coding)
+                self.text=response.read().decode(self.url)
+                data = response.read().decode(self.coding)
+                print("Fetched %uB from %s." % (len(data),self.url))
+                self.text = data.replace(b"\n", b"")
+                return True
             else:
-                # Get encoding, try utf-8 if not found.
-                self.coding = response.headers.get_content_charset()
-                if (self.coding is None):
-                    self.coding = "utf-8"
-
-                # Get data as string
-                if (len(response.read()) != 0):
-                    print("Encoding is %s" % self.coding)
-                    self.text=response.read().decode(self.url)
-                    data = response.read().decode(self.coding)
-                    print("Fetched %uB from %s." % (len(data),self.url))
-                    self.text = data.replace(b"\n", b"")
-                    return True
-                else:
-                    print("No data for %s !" % (self.url))
-                    return False
+                print("No data for %s !" % (self.url))
+                return False
 
         "Set HTML data to parse"
         def setHtmlData(self, newContent):
