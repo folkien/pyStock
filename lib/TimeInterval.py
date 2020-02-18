2b98684f29b247198502a7aeb5006cc43483f8d1
Sławomir Paszko <folkus@gmail.com>
2020-02-18 22:37:16 +0100

    Time interval class added.'

---
+++ lib/TimeInterval.py
@@ -0,0 +1,22 @@
+import datetime
+
+executionIntervals = [ "monthly", "weekly", "daily"]
+
+def GetIntervalBegin(intervalName):
+    if (intervalName in  executionIntervals):
+        today = datetime.datetime.now()
+
+        if (intervalName == "monthly"):
+            interval = today - datetime.timedelta(days=30)
+        elif (intervalName == "weekly"):
+            interval = today - datetime.timedelta(days=7)
+        else:
+            interval = today - datetime.timedelta(days=1)
+
+        return interval
+
+class TimeInterval:
+    
+    def __init__(self):
+        self.intervals = [ "monthly", "weekly", "daily"]
+        
