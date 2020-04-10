b0d7125b9685454629a88fa3780fd9605cd44e5c
Sławomir Paszko <folkus@gmail.com>
2020-04-10 23:46:04 +0200

    Added 90 and 10 lines to MFI. Added export of signals.

--- lib/moneyflow.py
+++ lib/moneyflow.py
@@ -23,11 +23,15 @@ class MoneyFlow:
             self.moneyMarket = self.moneyFlow.cumsum() 
 
             # Signals
-            # TODO: change to 90 and 10
             fromBottom,fromTop=FindIntersections(self.mfi,20)
             self.buy  = fromBottom
             fromBottom,fromTop=FindIntersections(self.mfi,80)
             self.sell = fromTop
+            # TrenToFall / TrendToRise
+            fromBottom,fromTop=FindIntersections(self.mfi,10)
+            self.buyStrong  = fromBottom
+            fromBottom,fromTop=FindIntersections(self.mfi,90)
+            self.sellStrong = fromTop
 
         # returns AverageTrueRange
         def GetMoneyFlow(self):
@@ -36,7 +40,6 @@ class MoneyFlow:
         # Set MoneyFlow indicator
         def InitMoneyFlow(self, tp, volume, n):
             moneyFlow = tp * volume
-
             posFlow = pd.Series()
             negFlow = pd.Series()
 
@@ -56,9 +59,10 @@ class MoneyFlow:
 
         # Export indicator signals to report
         def ExportSignals(self, reportSignals):
-            return 0
-#             reportSignals.AddDataframeSignals(self.buy,"MoneyFlow","buy")
-#             reportSignals.AddDataframeSignals(self.sell,"MoneyFlow","sell")
+            reportSignals.AddDataframeSignals(self.buy, "MFI","buy")
+            reportSignals.AddDataframeSignals(self.sell,"MFI","sell")
+            reportSignals.AddDataframeSignals(self.buyStrong, "MFI","buyStrong")
+            reportSignals.AddDataframeSignals(self.sellStrong,"MFI","sellStrong")
 
         # Plot method
         def PlotMoneyFlow(self,ax):
@@ -85,19 +89,32 @@ class MoneyFlow:
             plt.plot(overBought.index, overBought, '--', label='Overbought', linewidth=1.0, color = '#940006')
 #             plt.fill_between(x_axis, self.mfi, overBought['value'], 
 #                              where=self.mfi>overBought.values,color='#ffb3b3')
+            # OverBought - Gene Quong and Avrum Soudack
+            overBought = CreateDataLine(self.mfi.index, 90, 90)
+            plt.plot(overBought.index, overBought, '--', linewidth=0.6, color = '#940006')
+
             #OverSold
             overSold = CreateDataLine(self.mfi.index, 20, 20, True)
             plt.plot(overSold.index, overSold, '--', label='Oversold', linewidth=1.0, color = '#169400')
 #             plt.fill_between(x_axis, self.mfi, overSold['value'], 
 #                              where=self.mfi<overSold.values,color='#b3ffb3')
+            # OverSold - Gene Quong and Avrum Soudack
+            overSold = CreateDataLine(self.mfi.index, 10, 10)
+            plt.plot(overSold.index, overSold, '--', linewidth=0.6, color = '#169400')
 
 #             # Signals plottting
             if (self.buy is not None and self.buy.size):
                 plt.plot(self.buy.index, self.buy, 'o', color = '#000000', ms=8)
                 plt.plot(self.buy.index, self.buy, 'o', label='Buy', color = '#00FF00')
+            if (self.buyStrong is not None and self.buyStrong.size):
+                plt.plot(self.buyStrong.index, self.buyStrong, 's', color = '#000000', ms=8)
+                plt.plot(self.buyStrong.index, self.buyStrong, 's', label='BuyStrong', color = '#00FF00')
             if (self.sell is not None and self.sell.size):
                 plt.plot(self.sell.index, self.sell, 'o', color = '#000000', ms=8)
                 plt.plot(self.sell.index, self.sell, 'o', label='Sell', color = '#FF0000')
+            if (self.sellStrong is not None and self.sellStrong.size):
+                plt.plot(self.sellStrong.index, self.sellStrong, 's', color = '#000000', ms=8)
+                plt.plot(self.sellStrong.index, self.sellStrong, 's', label='SellStrong', color = '#FF0000')
 
             # Limits of plot
             plt.ylim(top=100,bottom=0)
