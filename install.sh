39e6779661cdc16af370bf675d105ff13168b00d
Sławomir Paszko <folkus@gmail.com>
2020-04-29 14:14:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,10 @@ dir=$(pwd)
 
 stock-alarms -c > /dev/null
 if [ $? -ne 0 ]; then
- pip install pandas-datareader
- pip install pandas
- pip install matplotlib
- sudo apt-get install python-tk
- location=$(pip show pandas | grep Locat | grep " .*" -o)
- file=$(find ${location} | grep "/stooq.py$")
- gvim ${file}
+ echo "Please install requirements.txt."
 fi
 
- # commands install
+ # commands install to linux /usr/bin/
  cd /usr/bin
  sudo ln -sf ${dir}/stock-viewer.py stock-viewer
  sudo ln -sf ${dir}/scripts/stock-manager.sh stock-manager
