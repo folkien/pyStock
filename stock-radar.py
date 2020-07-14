4091db424ab9a44376b629eb51e0f2fbeede57d3
Sławomir Paszko <folkus@gmail.com>
2020-07-15 00:36:53 +0200

    Stock radar updates.

--- stock-radar.py
+++ stock-radar.py
@@ -46,6 +46,7 @@ def BiznesRadarParse(content):
     data = data.reset_index()
     data = data.rename(columns={'Cena / Wartość księgowa': 'C/WK',
                                 'Cena / Przychody ze sprzedaży': 'C/P',
+                                'Cena / Zysk': 'C/Z',
                                 'Aktualny kurs': 'Kurs',
                                 'Średni obrót z 5 sesji [zł]': 'Obrot',
                                 'Piotroski F-Score': 'Piotroski',
@@ -66,34 +67,75 @@ def BiznesRadarParse(content):
 def Filter(stocks):
     ''' Filter and sort stocks from pandas dataframe'''
     ratings = []
+    comments = []
+
     # Add column with spasz value
     for i in (range(len(stocks['Profil']))):
-        rating = stocks['ROE'][i]*1.5 +\
-            stocks['ROA'][i]*1.5 +\
-            (stocks['Piotroski'][i]*100)/9
+        commentary = ''
+        rating = 0
+
+        if 'ROE' in stocks:
+            rating += stocks['ROE'][i]*1.5
+
+        if 'ROA' in stocks:
+            rating += stocks['ROA'][i]*1.5
+
+        if 'Piotroski' in stocks:
+            rating += ((stocks['Piotroski'][i]*100)/9)
+
+        # C/P
+        if 'C/P' in stocks:
+            value = stocks['C/P'][i]
+            if (value < 1):
+                rating += 100 - 25*value
+            elif (value < 10):
+                rating += 80 - 5*value
+            elif (value < 100):
+                rating += 33.3 - 0.33*value
+            else:
+                rating += 0
+
+        # C/Z
+        if 'C/Z' in stocks:
+            value = stocks['C/Z'][i]
+            if (value < 1):
+                rating += 100 - 25*value
+            elif (value < 10):
+                rating += 80 - 5*value
+            elif (value < 100):
+                rating += 33.3 - 0.33*value
+            else:
+                rating += 0
+
         # C/WK
-        c_wk = stocks['C/WK'][i]
-        if (c_wk < 1):
-            rating += 100 - 25*c_wk
-        elif (c_wk < 10):
-            rating += 80 - 5*c_wk
-        elif (c_wk < 100):
-            rating += 33.3 - 0.33*c_wk
-        else:
-            rating += 0
+        if 'C/WK' in stocks:
+            c_wk = stocks['C/WK'][i]
+            if (c_wk < 1):
+                rating += 100 - 25*c_wk
+            elif (c_wk < 10):
+                rating += 80 - 5*c_wk
+            elif (c_wk < 100):
+                rating += 33.3 - 0.33*c_wk
+            else:
+                rating += 0
+
         # Obrot
-        obrot = stocks['Obrot'][i]
-        if (obrot < 1000):
-            rating += 0
-        elif (obrot < 10000):
-            rating += 20
-        elif (obrot < 100000):
-            rating += 60
-        else:
-            rating += 100
+        if 'Obrot' in stocks:
+            obrot = stocks['Obrot'][i]
+            if (obrot < 1000):
+                rating += 0
+            elif (obrot < 10000):
+                rating += 20
+            elif (obrot < 100000):
+                rating += 60
+            else:
+                rating += 100
+
         ratings.append(rating)
+        comments.append(commentary)
 
     stocks['Rating'] = ratings
+    stocks['Comments'] = commentary
     stocks = stocks.sort_values(by=['Rating'], ascending=False)
     return stocks
 
